79db016c50a74c108b5fb0b0ae0285d6e41afd90
Mochamad Fahmi <151912818+FahmiMoch@users.noreply.github.com>
2025-08-07 10:47:15 +0700

    Update menu-navigation.js
    Penggunaan Export

--- js/menu-navigation.js
+++ js/menu-navigation.js
@@ -1,6 +1,8 @@
-function toggleMenu() {
-          const items = document.querySelectorAll('#fabMenu .menu-item');
-          items.forEach(item => {
-            item.classList.toggle('hidden');
-          });
-        }
+export function toggleMenu() {
+  const items = document.querySelectorAll('#fabMenu .menu-item');
+  items.forEach(item => {
+    item.classList.toggle('hidden');
+  });
+}
+
+
